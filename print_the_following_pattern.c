d0640578e6de536a1df2ccb6459197f663ab7682
Jyothiswaroopa5 <119839745+Jyothiswaroopa5@users.noreply.github.com>
2022-12-07 10:54:29 +0530

    #include<stdio.h> int main() {     int n,i,j;     scanf("%d",&n);     for(i=n;i>=1;i--)     {         for(j=1;j<=i;j++)         printf("%d",j);         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,14 +1,13 @@
 #include<stdio.h>
-int  main()
+int main()
 {
     int n,i,j;
     scanf("%d",&n);
     for(i=n;i>=1;i--)
     {
-        for(j=n;j>=1;j--)
-        printf("%d ",j);
+        for(j=1;j<=i;j++)
+        printf("%d",j);
         printf("
 ");
-    }   
-    
+    }
 }
